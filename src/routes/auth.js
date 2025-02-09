c01b18d5d0473554624e85072f7a93986a1f6d90
BaluPasumarthi <balupasumarthi1@gmail.com>
2025-02-09 11:44:11 +0530

    fixed logout bug

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -56,8 +56,15 @@ authRouter.post("/login", async (req, res) => {
 });
 
 authRouter.post("/logout", async (req, res) => {
-  res.cookie("token", null, { expires: new Date(Date.now()), httpOnly: true });
-  res.json({ message: "Logout Successful" });
+  res.cookie("token", "", {
+    httpOnly: true,
+    secure: true, 
+    sameSite: "strict",
+    expires: new Date(0), 
+  });
+
+  res.status(200).json({ message: "Logout Successful" });
 });
+
 
 module.exports = authRouter;
