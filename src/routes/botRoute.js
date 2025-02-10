ec10a17b12479ed02c3e1c82a1f0030e8454f02a
BaluPasumarthi <balupasumarthi1@gmail.com>
2025-02-10 17:21:46 +0530

    added chatbot features

--- src/routes/botRoute.js
+++ src/routes/botRoute.js
@@ -1,13 +1,31 @@
 const express = require("express");
-const { getChatbotResponse } = require("../utils/chatbot");
 const botRouter = express.Router();
+const { userAuth } = require("../middleware/auth");
+const chatbotModel = require("../models/chatbot");
 
 
-botRouter.post("/chatbot/message", (req, res) =>{
+botRouter.post("/chatbot/message", async (req, res) =>{
 
-    const {message} = req.body;
-    const response = getChatbotResponse(message);
-    res.json({response});
+    try{
+        const {question} = req.body;
+        const query = question.toLowerCase().trim();
+
+        const chatbotresponse = await chatbotModel.findOne({question:query});
+
+        if(chatbotresponse){
+            return res.json({response:chatbotresponse.response})
+        }
+        else{
+            return res.json({
+              response:
+                "Sorry, I don't understand that. Please try a different question.",
+            });
+        }
+    }
+    catch(err){
+        res.status(500).json({ error: "Server error" });
+    }
+
 });
 
 module.exports = botRouter;
