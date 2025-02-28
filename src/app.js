2346639da0dcc7cd2e5e672f3d3b8182a4b14ed9
BaluPasumarthi <balupasumarthi1@gmail.com>
2025-02-28 20:21:11 +0530

    added origin address

--- src/app.js
+++ src/app.js
@@ -6,8 +6,11 @@ require("dotenv").config();
 
 const app = express();
 app.use(
-  cors()
-);
+  cors({
+    origin: "http://localhost:5173", 
+    credentials: true, 
+  })
+)
 app.use(express.json());
 app.use(cookieParser());
 
