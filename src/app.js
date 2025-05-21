c241d03c48805d70bf3bfc9084454cdfe7c20fb3
Balupasumarthi1 <balu.pasumarthi@medixcy.com>
2025-05-21 16:16:26 +0330

    added cors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,14 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    credentials: true, 
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"], 
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
