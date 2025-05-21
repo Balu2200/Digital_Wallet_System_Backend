2dc7df5fd9b579bb7b82d5052d15546bc882d035
Balupasumarthi1 <balu.pasumarthi@medixcy.com>
2025-05-21 19:26:43 +0330

    bugs: cors

--- src/app.js
+++ src/app.js
@@ -6,14 +6,26 @@ require("dotenv").config();
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://pay-swift-frontend.vercel.app/",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173", 
-    credentials: true, 
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"], 
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 app.use(express.json());
 app.use(cookieParser());
